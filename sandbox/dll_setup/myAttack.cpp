d4bbe10bd6b6a4221eabc4e67bfe31df889aa51a
Raphael Norwitz <raphael.s.norwitz@gmail.com>
2018-02-27 02:56:32 -0800

    made progress on dll injection

--- sandbox/dll_setup/myAttack.cpp
+++ sandbox/dll_setup/myAttack.cpp
@@ -6,11 +6,23 @@
 #define EXPORTING_DLL
 // #include "myAttack.h"
 
-// extern "C" void HelloWorld(void);
+#define LOG_FILE L"C:\\Users\\IEUser\\SRML\\myAttack\\test\\Log.txt"
+
+int globval = 5;
+
+void WriteLog(char *text) {
+	HANDLE hfile = CreateFileW(LOG_FILE, GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	DWORD written;
+	WriteFile(hfile, text, strlen(text), &written, NULL);
+	WriteFile(hfile, "\r\n", 2, &written, NULL);
+	CloseHandle(hfile);
+}
 
 BOOL APIENTRY DllMain( HANDLE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved )
 {
-	std::cout < "at least it loaded\n";
+	// std::cout < "at least it loaded\n";
+	WriteLog("In business");
+	globval = 7;
    return TRUE;
 }
 
@@ -26,5 +38,5 @@ extern "C" __declspec(dllexport) int Test()
    /*MessageBox( NULL, TEXT("Hello World"), 
    TEXT("In a DLL"), MB_OK);*/
    // std::cout << "ran motherfucker!!\n";
-   return 12;
+   return globval;
 };
--- sandbox/dll_setup/testScript.cpp
+++ sandbox/dll_setup/testScript.cpp
@@ -12,8 +12,14 @@ HINSTANCE hinstDLL;
 DLLPROC HelloWorld;
 DLLPROCINT TestFunc;
 BOOL fFreeDLL;
+HANDLE victimProcess;
 
-int main() {
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		std::cout << "need additional args\n";
+		exit(1);
+	}
+	int pid = atoi(argv[1]);
 	hinstDLL = LoadLibrary("myAttack.dll");
 	if (hinstDLL == NULL) {
 		std::cout << "failed to load dll at all\n";
@@ -39,5 +45,26 @@ int main() {
 	}
 	fFreeDLL = FreeLibrary(hinstDLL);
 	printf("hello world\n");
+	
+	/* attempt dll injection into process in first arg */
+	
+	victimProcess = OpenProcess(PROCESS_ALL_ACCESS, 0, pid);
+	if (victimProcess == NULL) {
+		std::cout << "OpenProcess failed\n";
+		std::cout << GetLastError() << "\n";
+	}
+	
+	LPVOID allocEd = VirtualAllocEx(victimProcess, 0, 0x104, 0x3000, 4 );
+	if (allocEd == NULL) {
+		std::cout << "VirtualAllocEx failed\n";
+		std::cout << GetLastError() << "\n";
+	}
+	
+	SIZE_T out = 0;
+	char *name = "myAttack.dll";
+	
+	BOOL WrittenMemory = WriteProcessMemory(victimProcess, allocEd, name, strlen(name), &out);
+	
+	
 	return 0;
 }
