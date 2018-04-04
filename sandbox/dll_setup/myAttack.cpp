61a9be996e544f3555734a998ab5012c53a814ab
Raphael Norwitz <raphael.s.norwitz@gmail.com>
2018-04-04 10:49:24 -0700

    demonstrated patch in live memory!!

--- sandbox/dll_setup/myAttack.cpp
+++ sandbox/dll_setup/myAttack.cpp
@@ -2,6 +2,7 @@
 
 #include <windows.h>
 #include <iostream>
+#include <string>
 //#include "user32.h"
 #define EXPORTING_DLL
 // #include "myAttack.h"
@@ -9,6 +10,8 @@
 #define LOG_FILE L"C:\\Users\\IEUser\\SRML\\myAttack\\test\\Log.txt"
 
 int globval = 5;
+
+int test_ptr = 0x004010B9;
 
 void WriteLog(char *text) {
 	HANDLE hfile = CreateFileW(LOG_FILE, GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -21,8 +24,14 @@ void WriteLog(char *text) {
 BOOL APIENTRY DllMain( HANDLE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved )
 {
 	// std::cout < "at least it loaded\n";
-	WriteLog("In business");
+	char b[2]; //= *test_ptr;
+	b[1] = '\0';
+	b[0] = *((char *)test_ptr);
+	*((char *) test_ptr) = 'b';
+	// string myStr = b;
+	WriteLog(b);
 	globval = 7;
+	
    return TRUE;
 }
 
