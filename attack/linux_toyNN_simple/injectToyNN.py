fe5516954368715b7c10f3899b98b524801be835
Raphael Norwitz <raphael.s.norwitz@gmail.com>
2018-05-04 11:34:36 -0400

    added citation

--- attack/linux_toyNN_simple/injectToyNN.py
+++ attack/linux_toyNN_simple/injectToyNN.py
@@ -1,20 +1,7 @@
-''''
-import re
-maps_file = open("/proc/self/maps", 'r')
-mem_file = open("/proc/self/mem", 'r', 0)
-for line in maps_file.readlines():  # for each mapped region
-    m = re.match(r'([0-9A-Fa-f]+)-([0-9A-Fa-f]+) ([-r])', line)
-    if m.group(3) == 'r':  # if this is a readable region
-        start = int(m.group(1), 16)
-        end = int(m.group(2), 16)
-        mem_file.seek(start)  # seek to region start
-        chunk = mem_file.read(end - start)  # read region contents
-        print chunk,  # dump contents to standard output
-maps_file.close()
-mem_file.close()
-
-'''
-
+# attack on toyNN on linux 
+# copied from:
+# https://unix.stackexchange.com/questions/6267/how-to-re-load-all-running-applications-from-swap-space-into-ram/6271#6271
+ 
 #!/usr/bin/env python
 import ctypes, re, sys
 
